5cc2c9a51b0243c1947f7c90f24873e790606b7e
Micah Harker <micah.harker@gmail.com>
2024-02-08 15:40:35 -0500

    Create parser class

---
+++ src/net/Parser.java
@@ -0,0 +1,11 @@
+package net;
+
+public class Parser {
+
+    //gets instantiated by pc/switch
+
+    public void parseConfig(String id){
+        //this method will get called and the argument passed to this method will be the id of the node using it
+        //it will return the node's neighbors, and their ip's and real ports
+    }
+}
