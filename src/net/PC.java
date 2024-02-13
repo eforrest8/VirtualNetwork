b9c1f8b8a3621ade7b7afb8da93a79cb11613471
hooperethan123 <emhooper@bsu.edu>
2024-02-13 18:49:37 -0500

    add basic udp packet sending

--- src/net/PC.java
+++ src/net/PC.java
@@ -1,21 +1,51 @@
 package net;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PC {
     private int MAC;
-
+    public static void main(String[] args) throws Exception {
     //uses parser to read config
 
     //we're expected to use threads so these can run concurrently
-    public void send(){
+
+        if(args.length != 2){
+            System.out.println("Syntax: EchoClient <serverIP> <serverPort>");
+            return;
+        }
+        InetAddress serverIP = InetAddress.getByName(args[0]);
+        int serverPort = Integer.parseInt(args[1]);
+
+        System.out.println("Type your message below.");
+        Scanner keyboard = new Scanner(System.in);
+
+        String message = keyboard.nextLine();
+        DatagramSocket socket = new DatagramSocket();
+        DatagramPacket request = new DatagramPacket(message.getBytes(),
+                message.getBytes().length,
+                serverIP,
+                serverPort
+        );
+        socket.send(request);
+        socket.close();
+
+
         //create virtual frame for udp payload
         //this will include source mac, message, dest mac
-    }
 
-    public void createFrame(){}
+        //public void createFrame(){}
 
-    public void receive(){
+        //public void receive(){
+
         //reads to check if the packet is intended for this pc
     }
-
-
 }
+
+
--- src/net/Switch.java
+++ src/net/Switch.java
@@ -1,15 +1,38 @@
 package net;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
 public class Switch {
+    public static void main(String[] args) throws Exception{
     // use parser to read config
 
-    public void readTable(){}
+    //public void readTable(){}
 
-    public void addToTable(){}
+    //public void addToTable(){}
 
-    public void flood(){}
+    //public void flood(){}
 
-    public void send(){}
+    //public void send(){}
 
-    public void receive(){}
+        DatagramSocket serverSocket = new DatagramSocket(3000);
+        DatagramPacket clientRequest = new DatagramPacket(
+                new byte[1024],
+                1024);
+        serverSocket.receive(clientRequest);
+        byte[] clientMessage = Arrays.copyOf(
+                clientRequest.getData(),
+                clientRequest.getLength()
+        );
+
+        String clientResponse = new String(clientMessage);
+        System.out.println(clientResponse);
+        serverSocket.close();
+    }
 }
+
+
