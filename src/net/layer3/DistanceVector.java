1e9e77826295930c657cc3815da31b9528a2c7ed
Ethan Forrest <eforrest8@outlook.com>
2024-03-19 15:42:22 -0400

    Add toBytes() method

--- src/net/layer3/DistanceVector.java
+++ src/net/layer3/DistanceVector.java
@@ -1,5 +1,6 @@
 package net.layer3;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,21 @@ public class DistanceVector {
      * Mapping of subnets to routes
      */
     Map<String, Route> distances = new HashMap<>();
+
+    byte[] toBytes() {
+        ByteBuffer buf = ByteBuffer.allocate(byteLength());
+        return buf.array();
+    }
+
+    private int byteLength() {
+        return distances.entrySet().stream()
+                .mapToInt(e -> e.getKey().length() + e.getValue().byteLength())
+                .sum();
+    }
 }
 
-record Route(int distance, String nextHop) {}
+record Route(int distance, String nextHop) {
+    public int byteLength() {
+        return 4 + nextHop().length();
+    }
+}
