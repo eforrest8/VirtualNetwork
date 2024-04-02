158ed014656292c76cb9a41f143ebcd547de5c14
Ethan Forrest <eforrest8@outlook.com>
2024-04-02 16:29:50 -0400

    Add boolean return to updateRecord()

--- src/net/layer3/DistanceVector.java
+++ src/net/layer3/DistanceVector.java
@@ -34,10 +34,26 @@ public class DistanceVector {
         }
     }
 
+    /**
+     * <h1>lol i'm doing this in the most unreadable way imaginable</h1>
+     * <p>
+     *     here's a simplification of the code:
+     * </p>
+     *     {@code !distances.get(subnet).equals(...) || distances.putIfAbsent(...) == null}
+     * <p>
+     *     this is a boolean OR. the left term is true when the old route
+     *     to this subnet is not equal to the one we get after running the
+     *     inner portion. (i.e. the route changed.)
+     *     the right term is true when no route was previously
+     *     associated with this subnet. (i.e. the route changed.)
+     *     If either is true, we return true; otherwise we return false.
+     * </p>
+     * <p>i hope my logic here is correct >.<</p>
+     */
     public boolean updateRecord(String subnet, Route route) {
-        distances.computeIfPresent(subnet, (key, oldRoute) ->
-                oldRoute.distance() <= route.distance() ? oldRoute : route);
-        distances.putIfAbsent(subnet, route);
+        return !distances.get(subnet).equals(distances.computeIfPresent(subnet, (key, oldRoute) ->
+                oldRoute.distance() <= route.distance() ? oldRoute : route)) ||
+        distances.putIfAbsent(subnet, route) == null;
     }
 
     public boolean merge(DistanceVector other, String sender) {
